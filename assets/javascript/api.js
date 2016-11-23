641b9a4782512037100f6820960c96fe169906ab
Anthony Myhre <anthony.myhre@utexas.edu>
2016-11-22 23:51:06 -0600

    buttons were not working after adding a button b/c the event listener was not attached to dynamically created buttons. Have to select a parent element to be able to do event listeners on dynamically created elements.

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,7 +1,43 @@
 $(document).ready(function() {
-	var topics = ["Will Ferrell", "Will Smith", "Julia Louis-Dreyfus", "Tom Cruise", "Steve Carell", "John Cleese", "Amy Poehler", "Dana Carvey"];
+	var topics = ["Will Ferrell",
+		"Will Smith",
+		"Julia Louis-Dreyfus",
+		"Tom Cruise",
+		"Steve Carell",
+		"John Cleese",
+		"Amy Poehler",
+		"Dana Carvey",
+		"Chris Pratt",
+		"Wayne Knight",
+		"Alfonso Ribeiro"];
 	
 	createButtons();
+
+	function apiCall() {
+		var actor = $(this).data('name');
+		var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=10&q=" + actor + "&fmt=json";
+		$.ajax({
+				url: queryURL,
+				method: "GET"})
+			.done(function(response) {
+				for (var i = 0; i < response.data.length; i++) {
+					console.log(response.data[i]);
+					var gifDiv = $('<div class="gif">');
+					// var rating = $('<p>').text("Rating: " + response.data[i].rating);
+					var rating = "<p>Rating: " + response.data[i].rating + "</p>";
+					console.log(rating);
+					var gif = "<img class='gifImg' src='" + response.data[i].images.fixed_height.url
+						 + "' alt='" + actor + "' data-state='animated' data-animated='" + response.data[i].images.fixed_height.url 
+						 + "' data-still='" + response.data[i].images.fixed_height_still.url + "'>";
+					gifDiv.append(rating + gif);
+					$('#giphyHolder').prepend(gifDiv);
+				}
+			});
+	}
+
+
+	$(document).on('click', '.actor', apiCall);
+
 
 	function createButtons() {
 		// empty the button holder
@@ -14,36 +50,14 @@ $(document).ready(function() {
 		*/
 		for (var i = 0; i < topics.length; i++) {
 			var btn = $('<button>');
-			btn.addClass('btn actor');
+			btn.addClass('btn btn-warning actor');
 			btn.attr('data-name', topics[i]);
 			btn.text(topics[i]);
 			$('#buttonHolder').append(btn);
 		}
 	}
 
-	$('.actor').on('click', function() {
-		var actor = $(this).data('name');
-		var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=10&q=" + actor + "&fmt=json";
-		$.ajax({
-				url: queryURL,
-				method: "GET"})
-			.done(function(response) {
-				for (var i = 0; i < response.data.length; i++) {
-					console.log(response.data[i]);
-					var gifDiv = $('<div class="gif">');
-					var rating = "<p>Rating: " + response.data[i].rating + "</p>";
-					console.log(rating);
-					var gif = "<img class='gifImg' src='" + response.data[i].images.fixed_height.url
-						 + "' alt='" + actor + "' data-state='animated' data-animated='" + response.data[i].images.fixed_height.url 
-						 + "' data-still='" + response.data[i].images.fixed_height_still.url + "'>";
-					gifDiv.append(rating + gif);
-					$('#giphyHolder').prepend(gifDiv);
-				}
-			});
-
-	});
-
-	$('.gifImg').on('click', function() {
+	$(document).on('click', '.gifImg', function() {
 		if ($(this).attr('data-state') == "animated") {
 			$(this).attr('data-state', 'still');
 			$(this).attr('src', $(this).data('still'));
@@ -60,7 +74,8 @@ $(document).ready(function() {
 	$('#addActor').on('click', function() {
 		var input = $('#actor-input').val().trim();
 		if ((input != "") && (topics.indexOf(input) == -1)) {
-			topics.push($('#actor-input').val().trim());
+			topics.push(input);
+			// console.log(input);
 			createButtons();
 		}
 		else if (input == "") {
@@ -69,7 +84,7 @@ $(document).ready(function() {
 		else {
 			alert("Sorry! A button for this actor/actress already exists. Please try again.");
 		}
+		$('#actor-input').val("");
 		return false;
 	});
-	// createButtons();
 });
